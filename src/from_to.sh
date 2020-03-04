542f51166df6c369d94d5956408b48d60245530b
brokkelkamp <rubenbrokkelkamp@gmail.com>
2020-03-04 15:45:17 +0100

    from to

---
+++ src/from_to.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+FROM=001
+UNTIL=199
+
+if [[ "$2" != "" ]]; then
+  FROM=$1
+  UNTIL=$2
+else
+  if [[ "$1" != "" ]]; then
+    UNTIL=$1
+  fi
+fi
+
+INPUTDIR="../input/exact/"
+OUTPUTDIR="../output/"
+GO=0
+
+
+echo "Computing treedepth of graphs $FROM until $UNTIL"
+
+for f in ${INPUTDIR}*; do
+    graph="${f##*/exact_}"
+    graph="${graph%.*}"
+    if [ "${graph}" = $FROM ] ; then
+      GO=1
+    fi
+
+    if [ "$GO" == 1 ] ; then
+      echo "Graph ${graph}"
+      ./main ${INPUTDIR}exact_${graph}.gr ${OUTPUTDIR}exact_${graph}.tree;
+    fi
+
+    if [ "${graph}" = "$UNTIL" ] ; then
+      break;
+    fi
+done  
+
+
