f705e302ed62c32caeb8aa1abedf3c1eeae6d7b2
Raymond van Venetie <rayvanve@gmail.com>
2020-05-13 21:58:56 +0200

    enable cache using a preprocessor flag

--- src/exact_cache.cpp
+++ src/exact_cache.cpp
@@ -1,17 +1,32 @@
 #include "exact_cache.hpp"
 
 #include "exact_caches/incbin.h"
+
+#if EXACT_CACHE_SIZE >= 2
 INCBIN(ExactCache2, "exact_caches/exact_cache_2.bin");
+#endif
+#if EXACT_CACHE_SIZE >= 3
 INCBIN(ExactCache3, "exact_caches/exact_cache_3.bin");
+#endif
+#if EXACT_CACHE_SIZE >= 4
 INCBIN(ExactCache4, "exact_caches/exact_cache_4.bin");
+#endif
+#if EXACT_CACHE_SIZE >= 5
 INCBIN(ExactCache5, "exact_caches/exact_cache_5.bin");
+#endif
+#if EXACT_CACHE_SIZE >= 6
 INCBIN(ExactCache6, "exact_caches/exact_cache_6.bin");
+#endif
+#if EXACT_CACHE_SIZE >= 7
 INCBIN(ExactCache7, "exact_caches/exact_cache_7.bin");
+#endif
+#if EXACT_CACHE_SIZE >= 8
 INCBIN(ExactCache8, "exact_caches/exact_cache_8.bin");
+#endif
 
 const std::vector<std::vector<int>> &exactCacheMapping(int N) {
   static std::array<std::vector<std::vector<int>>, exactCacheSize> mappings;
-  assert(N < exactCacheSize);
+  assert(N < EXACT_CACHE_SIZE);
   if (mappings[N].empty()) {
     mappings[N] = std::vector<std::vector<int>>(N, std::vector<int>(N, -1));
     int num = 0;
@@ -41,34 +56,48 @@ std::pair<int, int> exactCache(const std::vector<std::vector<int>> &adj) {
       return {0, 0};
     case 1:
       return {1, 0};
+#if EXACT_CACHE_SIZE >= 2
     case 2:
       assert(edges < gExactCache2Size);
       result = gExactCache2Data[edges];
       break;
+#endif
+#if EXACT_CACHE_SIZE >= 3
     case 3:
       assert(edges < gExactCache3Size);
       result = gExactCache3Data[edges];
       break;
+#endif
+#if EXACT_CACHE_SIZE >= 4
     case 4:
       assert(edges < gExactCache4Size);
       result = gExactCache4Data[edges];
       break;
+#endif
+#if EXACT_CACHE_SIZE >= 5
     case 5:
       assert(edges < gExactCache5Size);
       result = gExactCache5Data[edges];
       break;
+#endif
+#if EXACT_CACHE_SIZE >= 6
     case 6:
       assert(edges < gExactCache6Size);
       result = gExactCache6Data[edges];
       break;
+#endif
+#if EXACT_CACHE_SIZE >= 7
     case 7:
       assert(edges < gExactCache7Size);
       result = gExactCache7Data[edges];
       break;
+#endif
+#if EXACT_CACHE_SIZE >= 8
     case 8:
       assert(edges < gExactCache8Size);
       result = gExactCache8Data[edges];
       break;
+#endif
   }
   int td = result >> 4;
   int root = result & 15;
