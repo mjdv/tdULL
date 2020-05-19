760b8264e58b0bbe1aff7cb75795badfb97a71a3
Raymond van Venetie <rayvanve@gmail.com>
2020-05-19 13:56:47 +0200

    added

---
+++ src/sort_timing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for N in $(seq 1 5);
+do
+    rm treedepth_test.o
+    make SORTMETHOD=SORT$N > /dev/null
+    echo "SORT$N" > timings/SORT$N.csv
+    ./treedepth_test > timings/SORT$N.csv 2> /dev/null
+    printf "SORT$N total time took " && tail -1 timings/SORT$N.csv
+done
