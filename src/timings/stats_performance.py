146d13d84d5222f94ae2049bc81db347bf4bb235
Raymond van Venetie <rayvanve@gmail.com>
2020-05-19 17:44:21 +0200

    ready for hpc

---
+++ src/timings/stats_performance.py
@@ -0,0 +1,59 @@
+import sys
+from collections import defaultdict
+from glob import glob
+
+examplestats = defaultdict(list)
+filestats = {}
+
+fns_glob = glob('B*csv')
+for fn in fns_glob:
+    csv = open(fn)
+    #next(csv)
+    total_time = 0
+    total_solved = 0
+    for line in csv:
+        cols = line.split(',')
+        if len(cols) == 4:
+            example = cols[0]
+            td = int(cols[1])
+            time = float(cols[2])
+            if (td > -1):
+                total_time += time
+                total_solved += 1
+                examplestats[example].append((fn, td, time))
+    filestats[fn] = (total_time, total_solved)
+
+print('Example,Treedepth,{}'.format(','.join(
+    [fn.split('/')[0] for fn in fns_glob])),
+      file=sys.stderr)
+for example in sorted(examplestats.keys()):
+    stats = examplestats[example]
+    td_min = 99999
+    time_min = 9999
+    fns = {}
+    for fn, td, time in stats:
+        td_min = min(td, td_min)
+        fns[fn] = time
+    for fn, td, time in stats:
+        if td != td_min:
+            print('Error!? In {} the td was {} whereas the minimal td was {}.'.
+                  format(fn, td, td_min))
+    times = []
+    for fn in fns_glob:
+        if fn in fns:
+            times.append(fns[fn])
+        else:
+            times.append(-1)
+    #fns.sort(key=lambda fn: -filestats[fn][1])
+    print('{},{:<3},{}'.format(
+        example, td_min, ','.join([str(time).rjust(6) for time in times])),
+          file=sys.stderr)
+
+print('')
+print('File\t\t\t\t\tTotal Solved\tAverage time')
+fns_glob.sort(key=lambda fn: -filestats[fn][1])
+for fn in fns_glob:
+    total_time, total_solved = filestats[fn]
+    if total_solved:
+        print('{:<25}\t\t{}\t\t{}'.format(
+            fn.split('/')[0], total_solved, total_time / total_solved))
