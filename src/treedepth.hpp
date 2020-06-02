c5ead8d77b4f6e4e25bcdbaa9c15a69f2af8f2af
Raymond van Venetie <rayvanve@gmail.com>
2020-06-02 12:05:44 +0200

    submission stuff

--- src/treedepth.hpp
+++ src/treedepth.hpp
@@ -330,10 +330,6 @@ class Treedepth {
       auto separators = sep_generator.Next(100000);
 
       total_separators += separators.size();
-      if (G.N == full_graph.N)
-        std::cerr << "full_graph: generated total of " << total_separators
-                  << " separators so far." << std::endl;
-
       std::sort(separators.begin(), separators.end(),
                 [](const Separator &s1, const Separator &s2) {
                   return s1.largest_component < s2.largest_component;
@@ -346,7 +342,9 @@ class Treedepth {
                            store_best_separators);
 
         if (search_ubnd <= lower || search_lbnd >= upper || lower == upper) {
-          if (G.N == full_graph.N)
+          if (G.N == full_graph.N) {
+            std::cerr << "full_graph: generated total of " << total_separators
+                      << " separators so far." << std::endl;
             std::cerr << "full_graph: separator " << s << " / "
                       << separators.size()
                       << " gives `upper == lower == " << lower
@@ -356,6 +354,7 @@ class Treedepth {
                       << separator.largest_component.first << ", "
                       << separator.largest_component.second << ")."
                       << std::endl;
+          }
           // Choosing separator already gives us a treedepth decomposition which
           // is good enough (either a sister branch is at least this long, or it
           // matches a previously proved lower bound for this subgraph) so we
@@ -364,8 +363,11 @@ class Treedepth {
         }
       }
     }
-    if (G.N == full_graph.N)
+    if (G.N == full_graph.N) {
+      std::cerr << "full_graph: generated total of " << total_separators
+                << " separators so far." << std::endl;
       std::cerr << "full_graph: completed entire separator loop." << std::endl;
+    }
     node->lower_bound = lower = std::max(lower, new_lower);
     return {lower, upper, root};
   }
